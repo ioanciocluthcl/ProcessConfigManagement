1ffdd88288770b661b81014db90d4b9e826a8761
ioan.cioclut <ioan.cioclut@ness.com>
2022-12-12 15:18:58 +0200

    Initial commit

---
+++ src/test/java/com/drools/research/rulesmanagement/RulesmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drools.research.rulesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
